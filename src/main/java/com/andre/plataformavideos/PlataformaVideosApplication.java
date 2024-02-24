ecdc787d041cd45a8d07eadae911144f9f1b06be
Andre M. de Souza <andresouza44@gmail.com>
2024-02-24 19:20:23 -0300

    Projeto Criado

---
+++ src/main/java/com/andre/plataformavideos/PlataformaVideosApplication.java
@@ -0,0 +1,13 @@
+package com.andre.plataformavideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaVideosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaVideosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andre/plataformavideos/PlataformaVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.plataformavideos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaVideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
